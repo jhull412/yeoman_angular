282b944951a7cbe349a1c090e29376e883c1b301
Justin Hull <hulljc3@upmc.edu>
2015-08-09 21:50:16 -0400

    add find controller & view.

---
+++ app/scripts/controllers/find.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAppApp.controller:FindCtrl
+ * @description
+ * # FindCtrl
+ * Controller of the myAppApp
+ */
+angular.module('myAppApp')
+  .controller('FindCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
