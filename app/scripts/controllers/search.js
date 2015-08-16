473ad933ab5611f6f090fa2d1fb859501b8aa98d
Justin Hull <hulljc3@upmc.edu>
2015-08-16 00:23:45 -0400

    don't know what i'm doing

---
+++ app/scripts/controllers/search.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAppApp.controller:SearchCtrl
+ * @description
+ * # SearchCtrl
+ * Controller of the myAppApp
+ */
+angular.module('myAppApp')
+  .controller('SearchCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
